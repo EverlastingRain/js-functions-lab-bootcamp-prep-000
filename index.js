43a67a9590801114a58a1070eb589767c982df09
Tomasz Wiszowaty <twiszowaty@hotmail.com>
2019-09-29 15:24:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
   return "Happy holidays, you!";
 }
 
-function happyHolidayTo(holiday, name) {
+function happyHolidayTo(holiday) {
   return 'Happy ${holiday}, you!';
 }
